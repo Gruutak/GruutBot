5e6c01bde77e0679cb0de71bd2a7b77cefaa4c15
Lucas Leme Bertone <lucasbertz@hotmail.com>
2021-02-28 15:17:14 -0300

    Adding invite command

---
+++ commands/info/invite.go
@@ -0,0 +1,37 @@
+package info
+
+import (
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/gruutak/gruutbot/commands"
+)
+
+const permissions = "536084054"
+
+const urlFormat = "https://discord.com/api/oauth2/authorize?client_id=%s&permissions=%s&scope=bot"
+
+func init() {
+	cm := commands.Manager()
+
+	pc := &commands.Command{
+		Name:        "invite",
+		Description: "Shows the invite link for the bot",
+		Category:    commands.InfoCategory,
+		Command:     "invite",
+		Aliases:     []string{},
+		Run:         RunInvite,
+		Intent:      discordgo.IntentsGuildMessages,
+	}
+
+	cm.Register(pc)
+}
+
+func RunInvite(s *discordgo.Session, m *discordgo.MessageCreate, args ...string) (err error) {
+
+	url := fmt.Sprintf(urlFormat, s.State.User.ID, permissions)
+
+	_, err = s.ChannelMessageSendReply(m.ChannelID, url, m.MessageReference)
+
+	return
+}
